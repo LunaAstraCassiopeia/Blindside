e10121f6aff70734a9a0488a5776cb636cc329b8
Luna Astra Cassiopeia <kadabe380@gmail.com>
2025-12-21 11:30:33 +0800

    mantle

--- tags/mantle.lua
+++ tags/mantle.lua
@@ -2,7 +2,7 @@ SMODS.Tag {
     key = "mantle",
     hide_ability = false,
     atlas = 'bld_tag',
-    pos = {x = 4, y = 2},
+    pos = {x = 2, y = 4},
         in_pool = function(self, args)
             if G.GAME.selected_back.effect.center.config.extra then
                 if not G.GAME.selected_back.effect.center.config.extra.blindside then return false end
