5d9862380097c1636b76457d9b6c3dd90bb796ff
Luna Astra Cassiopeia <kadabe380@gmail.com>
2026-01-16 16:13:52 +0800

    bedazzler fix

--- relics/20meteor.lua
+++ relics/20meteor.lua
@@ -12,7 +12,6 @@ SMODS.Tag {
     apply = function(self, tag, context)
         if context.type == 'use_consumeable' and context.consumeable.ability.set == 'bld_obj_mineral'  then
             if SMODS.pseudorandom_probability(card, pseudoseed("flip"),1,2, 'flip') then
-                SMODS.calculate_context({using_consumeable = true, consumeable = context.consumeable, area = context.from_area})
                 tag:juice_up()
                 tag_area_status_text(tag, localize('k_again_ex'), G.C.FILTER, false, 0)
                 context.consumeable:use_consumeable(context.area)
