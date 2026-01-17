79e69f922d390f71a61005c1313d1672dd9e483a
Luna Astra Cassiopeia <kadabe380@gmail.com>
2026-01-17 22:13:01 +0800

    why does this contain cryptid code

--- lib/misc.lua
+++ lib/misc.lua
@@ -36,14 +36,6 @@ end
 local at = add_tag
 function add_tag(tag)
 	at(tag)
-	-- Update Costs for Clone Tag
-	if tag.name == "cry-Clone Tag" then
-		for k, v in pairs(G.I.CARD) do
-			if v.set_cost then
-				v:set_cost()
-			end
-		end
-	end
 	-- Make tags fit if there's more than 13 of them
 	-- This + Tag.remove Hook modify the offset to squeeze in more tags when needed
 	if #G.HUD_tags > 13 then
