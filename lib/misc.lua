bab25f07feec80fab50a096ea4de7a64bb46b174
Luna Astra Cassiopeia <kadabe380@gmail.com>
2026-01-22 13:34:08 +0800

    version bump

--- lib/misc.lua
+++ lib/misc.lua
@@ -13,7 +13,7 @@ function Game:main_menu(change_context)
                     n = G.UIT.T,
                     config = {
                         scale = 0.3,
-                        text = "Blindside BETA v0.2.1",
+                        text = "Blindside BETA v0.2.2-PLAYTEST",
                         colour = G.C.UI.TEXT_LIGHT
                     }
                 }
